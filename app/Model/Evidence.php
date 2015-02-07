fd181469a2740215f486d4e791fb38bad8dae618
aan <aansubarkah@gmail.com>
2015-02-07 09:35:09 +0700

    initial

--- app/Model/Evidence.php
+++ app/Model/Evidence.php
@@ -80,7 +80,48 @@ class Evidence extends AppModel
         )
     );
 
-    public function createZip($activityId = null) {
+    /**
+     * @param null $data
+     * @return bool
+     * @throws Exception
+     *
+     * $data array consist of
+     * name, type_id, uploader_id, activity_id, filename
+     */
+    public function saveAndRename($data = null)
+    {
+        App::uses('File', 'Utility');
+        $ret = false;
+        $filePath = WWW_ROOT . 'files' . DS . $data['filename'];
+        $file = new File($filePath, false, 777);
+
+        if ($file->exists()) {
+            //get file extension
+            $ext = $file->ext();
+            $ext = (string)$ext;
+            $ext = strtolower($ext);
+
+            //save to db
+            $data['extension'] = $ext;
+
+            $this->create();
+
+            if ($this->save($data)) {
+                //rename uploaded file with evidences table id
+                $filePathNew = WWW_ROOT . 'files' . DS . $this->getInsertID() . '.' . $ext;
+                rename($filePath, $filePathNew);
+                //create zip file
+                if($this->createZip($data['activity_id'])) $ret = true;
+            }
+        }
+
+        $file->close();
+
+        return $ret;
+    }
+
+    public function createZip($activityId = null)
+    {
         $files = $this->find('all', array(
             'recursive' => -1,
             'conditions' => array(
